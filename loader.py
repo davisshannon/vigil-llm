a9b6b4b0f036ce8efe08696d46a4536c2cbdf4c6
James Hodgkinson <james@terminaloutcomes.com>
2023-11-28 14:31:56 +1000

    chasing down lints and mypy things

--- loader.py
+++ loader.py
@@ -1,43 +1,36 @@
-import os
-import sys
 import argparse
-
-from loguru import logger
+import sys
+from loguru import logger  # type: ignore
 
 from vigil.core.config import Config
 from vigil.core.loader import Loader
-
 from vigil.vigil import setup_vectordb
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description='Load text embedding data into Vigil'
+    parser = argparse.ArgumentParser(description="Load text embedding data into Vigil")
+
+    parser.add_argument(
+        "-d", "--dataset", help="dataset repo name", type=str, required=False
     )
 
     parser.add_argument(
-        '-d', '--dataset',
-        help='dataset repo name',
-        type=str,
-        required=True
+        "-D", "--datasets", help="Specify multiple repos", type=str, required=False
     )
 
-    parser.add_argument(
-        '-c', '--config',
-        help='config file',
-        type=str,
-        required=True
-    )
+    parser.add_argument("-c", "--config", help="config file", type=str, required=True)
 
     args = parser.parse_args()
 
     conf = Config(args.config)
-
-    embedding_conf = conf.get_general_config().get('embedding', {})
-    scanner_conf = conf.get_scanner_config('vectordb')
-
-    logger.info(f'using database directory: {scanner_conf.get("db_dir")}')
-    vdb = setup_vectordb(scanner_conf, embedding_conf)
+    vdb = setup_vectordb(conf)
 
     data_loader = Loader(vector_db=vdb)
-    data_loader.load_dataset(args.dataset)
+    if args.datasets:
+        for dataset in args.datasets.split(","):
+            data_loader.load_dataset(dataset)
+    elif args.dataset:
+        data_loader.load_dataset(args.dataset)
+    else:
+        logger.error("Please specify a dataset or datasets!")
+        sys.exit(1)
